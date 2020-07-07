599d715f059a04f7e3aa244ddfcdf3d7aed29bd2
JulioAvalos <julioavalos2@msn.com>
2020-07-06 20:52:42 -0600

    cleaning up some console logs used to check our ingredients object transformation into an array

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -6,13 +6,10 @@ import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 const burger = (props) => {
     const transformedIngredients = Object.keys(props.ingredients)
         .map(igKey => {
-            console.log(igKey);
             return [...Array(props.ingredients[igKey])].map((_,i) => {
-                console.log(_,i, (igKey + i));
                 return <BurgerIngredient key={igKey + i} type={igKey} />
             }); 
         });
-    // console.log(transformedIngredients);
     return (
         <div className={classes.Burger}>
             <BurgerIngredient type="bread-top" />
