31a9690fc354ab19fa2788a2ddbdc0879aaa9b2f
JulioAvalos <julioavalos2@msn.com>
2020-07-08 22:52:47 -0600

    sending a post request

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -5,6 +5,7 @@ import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary'
+import axios from '../../axios-orders';
 
 const INGREDIENT_PRICES = {
     salad: 0.5,
@@ -78,7 +79,26 @@ class BurgerBuilder extends Component {
     }
 
     purchaseContinueHandler = () => {
-        alert('You continue!');
+        // alert('You continue!');
+        const order = {
+            ingredients: this.state.ingredients,
+            // as sample: for production, should calculate the price on the server, because users can manipulate it!
+            price: this.state.totalPrice, 
+            customer: {
+                name: 'Julio Avalos',
+                address: {
+                    street: 'testStreet2',
+                    zipCode: '00527',
+                    country: 'El Salvador'
+                },
+                email: 'test@test.com'
+            },
+            deliveryMethod: 'fastest'
+
+        };
+        axios.post('/orders.json', order)
+            .then(response => console.log(response))
+            .catch(error => console.log(error));
     }
 
     render(){
