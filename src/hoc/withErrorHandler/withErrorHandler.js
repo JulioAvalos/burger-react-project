ab9bf97e785c7b1507b56ea8f3f4d07d2005f477
JulioAvalos <julioavalos2@msn.com>
2020-07-08 23:40:44 -0600

    refactoring withErrorHandler name

--- src/hoc/withErrorHandler/withErrorHandler.js
+++ src/hoc/withErrorHandler/withErrorHandler.js
@@ -20,8 +20,6 @@ const withErrorHandler = (WrappedComponent, axios) => {
             });
         }
         
-       
-
         errorConfirmedHandler = () => {
             this.setState({error: null});
         }
