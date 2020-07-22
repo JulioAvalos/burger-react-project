c7f7380bef0fec5f00251d1a3280f4cd7bca5bdb
JulioAvalos <julioavalos2@msn.com>
2020-07-21 22:24:53 -0600

    finishing up testing components

--- src/components/Navigation/NavigationItems/NavigationItems.test.js
+++ src/components/Navigation/NavigationItems/NavigationItems.test.js
@@ -4,7 +4,7 @@ import { configure, shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
 import NavigationItems from './NavigationItems';
-import Navigationitem from './NavigationItem/NavigationItem';
+import NavigationItem from './NavigationItem/NavigationItem';
 
 configure({adapter: new Adapter()});
 
@@ -15,12 +15,19 @@ describe('<NavigationItems />', () => {
     });
 
     it('should render two <NavigationItem /> elements if not authenticated', () => {
-        expect(wrapper.find(Navigationitem)).toHaveLength(2);
+        expect(wrapper.find(NavigationItem)).toHaveLength(2);
     });
 
     it('should render three <NavigationItem /> elements if authenticated', () => {
         // wrapper = shallow(<NavigationItems isAuthenticated />);
         wrapper.setProps({isAuthenticated: true});
-        expect(wrapper.find(Navigationitem)).toHaveLength(3);
+        expect(wrapper.find(NavigationItem)).toHaveLength(3);
     });
+
+    it('should display a logout button ', () => {
+        // wrapper = shallow(<NavigationItems isAuthenticated />);
+        wrapper.setProps({isAuthenticated: true});
+        expect(wrapper.contains(<NavigationItem link="/logout">Logout</NavigationItem>)).toEqual(true);
+    });
+
 });
