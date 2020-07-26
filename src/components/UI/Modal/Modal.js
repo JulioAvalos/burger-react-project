f25326e99e86936fb3a97b08db0baef15f612a2e
JulioAvalos <julioavalos2@msn.com>
2020-07-25 20:03:07 -0600

    using react.memo() & more!

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,29 +1,33 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 import classes from './Modal.module.css';
 import Aux from '../../../hoc/Auxiliary/Auxiliary';
 import Backdrop from '../Backdrop/Backdrop';
 
-class Modal extends Component { //or we could use a PureComponent, but we dont really need to do many props checks in this one
+const modal = props => { //or we could use a PureComponent, but we dont really need to do many props checks in this one
     //this could be a function component, doesn't have to be a class
-    shouldComponentUpdate(nextProps, nextState) {
-        return nextProps.show !== this.props.show || nextProps.children !== this.props.children;
-    }
-    render () {
-        return (
-            <Aux>
-                <Backdrop show={this.props.show} clicked={this.props.modalClosed}/>
-                <div 
-                    className={classes.Modal}
-                    style={{
-                        transform: this.props.show ? 'translateY(0)' : 'translateY(-100vh)',
-                        opacity: this.props.show ? '1' : '0'
-                    }}>
-                    {this.props.children}
-                </div>
-            </Aux>
-        );
-    }
+
+    // shouldComponentUpdate(nextProps, nextState) {
+    //     return nextProps.show !== props.show || nextProps.children !== props.children;
+    // }
+    return (
+        <Aux>
+            <Backdrop show={props.show} clicked={props.modalClosed}/>
+            <div 
+                className={classes.Modal}
+                style={{
+                    transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
+                    opacity: props.show ? '1' : '0'
+                }}>
+                {props.children}
+            </div>
+        </Aux>
+    );
 }
 
-export default Modal;
+export default React.memo(
+    modal, 
+    (prevProps, nextProps) => 
+        nextProps.show === prevProps.show || 
+        nextProps.children === prevProps.children
+);
